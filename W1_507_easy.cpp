654cd4bbe6ba9fb49e4a0e43b2ff859d930698fc
Asher_Yang <a0009751@gmail.com>
2023-05-11 22:26:25 +0800

    Update W1_507_easy.cpp

--- W1_507_easy.cpp
+++ W1_507_easy.cpp
@@ -9,6 +9,22 @@ for(int i=2; i<=num/2; i++){
 }
 return sum==num;
 
+// Improve from way one
+if(num==1)
+    return false;
+int sum = 1;
+for(int i=2; i*i<=num; i++){
+    if(num%i==0){
+        if(i==num/i) //若該數為完全平方數則僅需要加一次
+            sum+=i;
+        else{        //否則配對的因數都需要相加起來
+            sum+=i;
+            sum+=num/i;
+        }
+    }
+}
+return sum==num;
+
 // second way
 //binary Search, 起始因數為2往下找若有配對成功即把left設成另一個因數, 這樣一次可以縮減許多範圍
 if(num==1)
